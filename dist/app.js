f8382b24e0e5bb2110d8a175382200c6bccf920c
Pavel Baksheev <sxeiter@mail.ru>
2025-03-17 12:59:34 +0500

    init

---
+++ dist/app.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.App = void 0;
+class App {
+    run() {
+        console.log('done!');
+    }
+}
+exports.App = App;
+const app = new App();
+app.run();
---
+++ src/app.ts
@@ -0,0 +1,8 @@
+export class App {
+    run() {
+        console.log('done!')
+    }
+}
+
+const app = new App();
+app.run();
